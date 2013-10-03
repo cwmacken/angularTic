66c31c4a97fcac94ae67b13000c4704c902cae1b
cwmacken <cwmacken@gmail.com>
2013-10-03 14:44:01 -0700

    winng values asigned

--- ticTacCtrl.js
+++ ticTacCtrl.js
@@ -3,10 +3,17 @@ $scope.ticTacToe= [['X','','O'],['','X',''],['','O','']];
   
 var turn=1
 $scope.clickSqr = function(row,column){
-	alert(event.target);
-	event.target.innerHTML="X"
-	alert('row');
-	alert('column')
+	if(turn % 2 ==1){
+		event.target.innerHTML="X";
+		event.target.value=1;
+	}
+	else{
+		event.target.innerHTML="O"
+		event.target.value=-1;	
+	}
+	turn++
 
-};}
+};
+
+}
 

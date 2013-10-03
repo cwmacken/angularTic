d875b116c92054bfc0316e0c85fa9b902d538bf0
cwmacken <cwmacken@gmail.com>
2013-10-03 15:44:47 -0700

    half working git logic

--- ticTacCtrl.js
+++ ticTacCtrl.js
@@ -1,7 +1,8 @@
 var TicTacCtrl = function($scope){
-$scope.ticTacToe= [['X','','O'],['','X',''],['','O','']];
+	$scope.ticTacToe= [['X','','O'],['','X',''],['','O','']];
   
 var turn=1
+
 $scope.clickSqr = function(row,column){
 	if(turn % 2 ==1){
 		event.target.innerHTML="X";
@@ -15,5 +16,102 @@ $scope.clickSqr = function(row,column){
 
 };
 
+$scope.check = function(row,column) {
+		
+		// asign a unique value to each unique box name 
+		var a= event.target.value;
+		var b = event.target.value;
+		var c = event.target.value;
+
+		var d = event.target.value;
+		var e = event.target.value;
+		var f = event.target.value;
+
+		var g = event.target.value;
+		var h = event.target.value;
+		var i = event.target.value;
+		
+
+	if(
+		(
+			(a+b+c==3) 
+		) 
+			|| 
+		(
+			(d+e+f==3)
+		)
+			|| 
+	  	(
+	  		(g+h+i==3) 
+	  	)
+	  		||
+	  	(
+	  		(a+d+g==3)
+	  	) 	
+	  		|| 
+	  	(
+	  		(b+e+h==3) 
+	  	)
+	  		|| 
+		( 
+			(c+f+i==3)
+		) 
+			||
+		(
+			(a+e+i==3)
+		) 
+			|| 
+		(
+			(c+e+g==3) 
+		)
+	)	{
+	  		alert('1p WINS')
+	  	}
+	 else if(
+		(
+			(a+b+c==-3)  
+		) 
+			|| 
+		(
+			(d+e+f==-3) 
+		)
+			|| 
+	  	(
+	  		(g+h+i==-3)
+	  	)
+	  		||
+	  	(
+	  		(a+d+g==-3) 
+	  	) 	
+	  		|| 
+	  	(
+	  		(b+e+h==-3) 
+	  	)
+	  		|| 
+		( 
+			(c+f+i==-3) 
+		) 
+			||
+		(
+			(a+e+i==-3) 
+		) 
+			|| 
+		(
+			(c+e+g==-3) 
+		)
+	)
+		{
+	  		alert('2p WINS')
+	  		
+	  	}
+
+
+	 else{
+	 	if(turn==10){
+	 		alert('DRAW.....')
+	 	}
+	 }
+}
+	
 }
 

02554c84f64f3da2a9c0588e72f1d601beaffecd
cwmacken <cwmacken@gmail.com>
2013-10-03 21:07:19 -0700

    beofre attmpting to add x or o option

--- ticTacCtrl.js
+++ ticTacCtrl.js
@@ -1,88 +1,141 @@
+var p1Name = 'Player 1';
+var p2Name = 'PLayer 2';
+
+function strGame(){
+	 document.getElementById('game').style.display="inline";
+	document.getElementById('startPage').style.display="none";
+ //  var p1Hold = document.getElementById('p1').value;
+ //  var p2Hold = document.getElementById('p2').value;
+ //  document.getElementById('x').innerHTML = (p1Hold.toUpperCase())+'\'S';
+ //  p1Name = p1Hold.toUpperCase();
+ //  p2Name = p2Hold.toUpperCase();
+	// return p1Name, p2Name;
+}
+function home(){
+  // var lookup=['a','b','c','d','e','f','g','h','i'];
+
+//   for(var z=0;z<=8;z++){
+   
+//         document.getElementById(lookup[z]).value=0;
+//         document.getElementById(lookup[z]).innerHTML="";
+        
+//         turn =1
+// };
+      document.getElementsByClassName('alertBox')[0].style.display = "none";
+      document.getElementById('game').style.display="none";
+      document.getElementById('startPage').style.display="inline";
+		reset();		
+}
+function reset(){
+	
+  // var lookup=['a','b','c','d','e','f','g','h','i'];
+
+  // for(var z=0;z<=8;z++){
+   
+  //       document.getElementById(lookup[z]).value=0;
+  //       document.getElementById(lookup[z]).innerHTML="";
+
+
+
+
+  // THIS ISNT WORKING ATM!!!
+
+
+
+
+        document.getElementsByClassName('alertBox')[0].style.display = "none";
+        document.getElementById('o').innerHTML = "TURN";
+        document.getElementById('x').innerHTML = p1Name+'\'S';
+        $scope.ticTacToe= [[0,0,0],[0,0,0],[0,0,0]];
+
+  };
+
+
 var TicTacCtrl = function($scope){
-	$scope.ticTacToe= [[0,0,0],[0,0,0],[0,0,0]];
-  
-var turn=1
+		$scope.ticTacToe= [[0,0,0],[0,0,0],[0,0,0]];
+	  
+	var turn=1
 
 
-$scope.clickSqr = function(row,column){
-	if ($scope.ticTacToe[row][column]==0) {							
-		if(turn % 2 ==1) {
-			event.target.innerHTML="X";
-			$scope.ticTacToe[row][column]=1;
+	$scope.clickSqr = function(row,column){
+		if ($scope.ticTacToe[row][column]==0) {							
+			if(turn % 2 ==1) {
+				event.target.innerHTML="X";
+				$scope.ticTacToe[row][column]=1;
+			}
+			else{
+				event.target.innerHTML="O"
+				$scope.ticTacToe[row][column]=-1;	
+			}
+			turn++
 		}
 		else{
-			event.target.innerHTML="O"
-			$scope.ticTacToe[row][column]=-1;	
-		}
-		turn++
+
+		};
 	}
-	else{
-
-	};
-}
-	
+		
 
 
-$scope.check = function(row,column) {
-		// asign a unique value to each unique box name 
-		var a = $scope.ticTacToe[0][0];
-		var b = $scope.ticTacToe[0][1];
-		var c = $scope.ticTacToe[0][2];
-		var d = $scope.ticTacToe[1][0];
-		var e = $scope.ticTacToe[1][1];
-		var f = $scope.ticTacToe[1][2];
-		var g = $scope.ticTacToe[2][0];
-		var h = $scope.ticTacToe[2][1];
-		var i = $scope.ticTacToe[2][2];
-	
+	$scope.check = function(row,column) {
+			// asign a unique value to each unique box name 
+			var a = $scope.ticTacToe[0][0];
+			var b = $scope.ticTacToe[0][1];
+			var c = $scope.ticTacToe[0][2];
+			var d = $scope.ticTacToe[1][0];
+			var e = $scope.ticTacToe[1][1];
+			var f = $scope.ticTacToe[1][2];
+			var g = $scope.ticTacToe[2][0];
+			var h = $scope.ticTacToe[2][1];
+			var i = $scope.ticTacToe[2][2];
+		
 
-	if(
-		((a+b+c==3)) 
-			|| 
-		((d+e+f==3))
-			|| 
-	  	((g+h+i==3))
-	  		||
-	  	((a+d+g==3)) 	
-	  		|| 
-	  	((b+e+h==3))
-	  		|| 
-		((c+f+i==3)) 
-			||
-		((a+e+i==3)) 
-			|| 
-		((c+e+g==3))
-	)	
-		{
-	  		alert('1p WINS')
-	  	}
-	 else if(
-		((a+b+c==-3)) 
-			|| 
-		((d+e+f==-3))
-			|| 
-	  	((g+h+i==-3))
-	  		||
-	  	((a+d+g==-3)) 	
-	  		|| 
-	  	((b+e+h==-3))
-	  		|| 
-		((c+f+i==-3)) 
-			||
-		((a+e+i==-3)) 
-			|| 
-		((c+e+g==-3))
-	)
-		{
-	  		alert('2p WINS')
-	  		
-	  	}
-	 else{
-	 	if(turn==10){
-	 		alert('DRAW.....')
-	 	}
-	 }
-}
-	
-}
+		if(
+			(a+b+c==3) 
+				|| 
+			(d+e+f==3)
+				|| 
+		  	(g+h+i==3)
+		  		||
+		  	(a+d+g==3) 	
+		  		|| 
+		  	(b+e+h==3)
+		  		|| 
+			(c+f+i==3) 
+				||
+			(a+e+i==3) 
+				|| 
+			(c+e+g==3)
+		)	
+			{
+		  		alert('1p WINS')
+		  	}
+		 else if(
+			(a+b+c==-3) 
+				|| 
+			(d+e+f==-3)
+				|| 
+		  	(g+h+i==-3)
+		  		||
+		  	(a+d+g==-3) 	
+		  		|| 
+		  	(b+e+h==-3)
+		  		|| 
+			(c+f+i==-3) 
+				||
+			(a+e+i==-3) 
+				|| 
+			(c+e+g==-3)
+		)
+			{
+		  		alert('2p WINS')
+		  		
+		  	}
+		 else{
+		 	if(turn==10){
+		 		alert('DRAW.....')
+		 	}
+		 }
+		}
+		
+	}
 

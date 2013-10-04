4ef3c4e515a13db36064786a6e6d60f3608d77f3
cwmacken <cwmacken@gmail.com>
2013-10-03 19:45:38 -0700

    commit before implementing old styles into ang project

--- ticTacCtrl.js
+++ ticTacCtrl.js
@@ -1,111 +1,82 @@
 var TicTacCtrl = function($scope){
-	$scope.ticTacToe= [['X','','O'],['','X',''],['','O','']];
+	$scope.ticTacToe= [[0,0,0],[0,0,0],[0,0,0]];
   
 var turn=1
 
+
 $scope.clickSqr = function(row,column){
-	if(turn % 2 ==1){
-		event.target.innerHTML="X";
-		event.target.value=1;
+	if ($scope.ticTacToe[row][column]==0) {							
+		if(turn % 2 ==1) {
+			event.target.innerHTML="X";
+			$scope.ticTacToe[row][column]=1;
+		}
+		else{
+			event.target.innerHTML="O"
+			$scope.ticTacToe[row][column]=-1;	
+		}
+		turn++
 	}
 	else{
-		event.target.innerHTML="O"
-		event.target.value=-1;	
-	}
-	turn++
 
-};
+	};
+}
+	
+
 
 $scope.check = function(row,column) {
-		
 		// asign a unique value to each unique box name 
-		var a= event.target.value;
-		var b = event.target.value;
-		var c = event.target.value;
-
-		var d = event.target.value;
-		var e = event.target.value;
-		var f = event.target.value;
-
-		var g = event.target.value;
-		var h = event.target.value;
-		var i = event.target.value;
-		
+		var a = $scope.ticTacToe[0][0];
+		var b = $scope.ticTacToe[0][1];
+		var c = $scope.ticTacToe[0][2];
+		var d = $scope.ticTacToe[1][0];
+		var e = $scope.ticTacToe[1][1];
+		var f = $scope.ticTacToe[1][2];
+		var g = $scope.ticTacToe[2][0];
+		var h = $scope.ticTacToe[2][1];
+		var i = $scope.ticTacToe[2][2];
+	
 
 	if(
-		(
-			(a+b+c==3) 
-		) 
+		((a+b+c==3)) 
 			|| 
-		(
-			(d+e+f==3)
-		)
+		((d+e+f==3))
 			|| 
-	  	(
-	  		(g+h+i==3) 
-	  	)
+	  	((g+h+i==3))
 	  		||
-	  	(
-	  		(a+d+g==3)
-	  	) 	
+	  	((a+d+g==3)) 	
 	  		|| 
-	  	(
-	  		(b+e+h==3) 
-	  	)
+	  	((b+e+h==3))
 	  		|| 
-		( 
-			(c+f+i==3)
-		) 
+		((c+f+i==3)) 
 			||
-		(
-			(a+e+i==3)
-		) 
+		((a+e+i==3)) 
 			|| 
-		(
-			(c+e+g==3) 
-		)
-	)	{
+		((c+e+g==3))
+	)	
+		{
 	  		alert('1p WINS')
 	  	}
 	 else if(
-		(
-			(a+b+c==-3)  
-		) 
+		((a+b+c==-3)) 
 			|| 
-		(
-			(d+e+f==-3) 
-		)
+		((d+e+f==-3))
 			|| 
-	  	(
-	  		(g+h+i==-3)
-	  	)
+	  	((g+h+i==-3))
 	  		||
-	  	(
-	  		(a+d+g==-3) 
-	  	) 	
+	  	((a+d+g==-3)) 	
 	  		|| 
-	  	(
-	  		(b+e+h==-3) 
-	  	)
+	  	((b+e+h==-3))
 	  		|| 
-		( 
-			(c+f+i==-3) 
-		) 
+		((c+f+i==-3)) 
 			||
-		(
-			(a+e+i==-3) 
-		) 
+		((a+e+i==-3)) 
 			|| 
-		(
-			(c+e+g==-3) 
-		)
+		((c+e+g==-3))
 	)
 		{
 	  		alert('2p WINS')
 	  		
 	  	}
-
-
 	 else{
 	 	if(turn==10){
 	 		alert('DRAW.....')
